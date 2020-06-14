53ae9ab0bc3d8e940e4b25401b90a5d600bac215
ThornstromDeveloper <thornstrom@gmail.com>
2020-06-14 17:59:50 +0200

    create a window object

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SDL.hpp"
+#include "window.hpp"
 
 //program entry point
 int main(int argc, char** argv)
@@ -7,6 +8,7 @@ int main(int argc, char** argv)
 	std::cout << "Start\n";
 
 	SDL::init();
+	Window* win = new Window();
 
 	std::cout << "Stopped\n";
 }
