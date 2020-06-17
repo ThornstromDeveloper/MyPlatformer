4850c6ec8cbdb296f768fe857211a6e95f328f5f
ThornstromDeveloper <thornstrom@gmail.com>
2020-06-18 01:59:14 +0200

    shapes object

---
+++ src/Shapes.cpp
@@ -0,0 +1,11 @@
+#include "Shapes.hpp"
+
+//Shapes constructor
+Shapes::Shapes()
+{
+}
+
+//Shapes destructor
+Shapes::~Shapes()
+{
+}
---
+++ src/Shapes.hpp
@@ -0,0 +1,14 @@
+#ifndef SHAPES_HPP
+#define SHAPES_HPP
+
+class Shapes
+{
+public:
+	Shapes();
+	~Shapes();
+
+private:
+
+};
+
+#endif //SHAPES_HPP
