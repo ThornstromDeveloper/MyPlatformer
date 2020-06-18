f7c092213415ca0c1c73d21a9b8c045258cb8820
ThornstromDeveloper <thornstrom@gmail.com>
2020-06-18 23:46:26 +0200

    game object box

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -2,12 +2,28 @@
 
 //GameObject constructor
 GameObject::GameObject(float x, float y, int w, int h):
-	position(nullptr)
+	position(nullptr),
+	box(nullptr)
 {
 	this->position = new Point(x, y);
+	this->box = new Rectangle(x, y, w, h);
 }
 
 //GameObject destructor
 GameObject::~GameObject()
 {
+	delete this->position;
+	delete this->box;
+}
+
+//get width of GameObject
+int GameObject::getWidth()
+{
+	return this->box->w;
+}
+
+//get height of GameObject
+int GameObject::getHeight()
+{
+	return this->box->h;
 }
--- src/GameObject.hpp
+++ src/GameObject.hpp
@@ -8,7 +8,10 @@ class GameObject
 public:
 	GameObject(float x = 0.0, float y = 0.0, int w = 0, int h = 0);
 	~GameObject();
+	int getWidth();
+	int getHeight();
 	Point* position;
+	Rectangle* box;
 };
 
 #endif //GAMEOBJECT_HPP
